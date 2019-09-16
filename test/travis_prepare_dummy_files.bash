810cb919c20bbb6a6048c6c0b743163c6c7ba827
KeiichiroYoshida <kyoshi2009@gmail.com>
2019-09-16 12:16:39 +0000

    Add touch /dev/rtlightsensor0, /dev/rtswith{0,1,2}

--- test/travis_prepare_dummy_files.bash
+++ test/travis_prepare_dummy_files.bash
@@ -2,7 +2,9 @@
 
 sudo touch /dev/rt{buzzer,motor,motoren,motor_raw_{1,r}}0
 sudo chmod 666 /dev/rt{buzzer,motor,motoren,motor_raw_{1,r}}0
+sudo touch /dev/rtlightsensor0
 echo "0 0 0 0" | sudo tee /dev/rtlightsensor0
 sudo chmod 666 /dev/rtlightsensor0
+sudo touch /dev/rtswitch{0,1,2}
 echo "0" | sudo tee /dev/rtswithch{0,1,2}
 sudo chmod 666 /dev/rtswitch{0,1,2}
